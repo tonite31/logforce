1153e25ce28e68fb5b4d6881f8dc3d7e62557792
rayim <rayim@creeta.com>
2019-03-26 22:12:13 +0900

    update

--- src/routes/logforce.stream.route.js
+++ src/routes/logforce.stream.route.js
@@ -7,6 +7,8 @@ module.exports = function(socket, logforceWeb)
         let origin = Buffer.from(data.log, 'hex');
         let decompressed = LZUTF8.decompress(origin);
 
+        console.log(JSON.parse(decompressed));
+
         logforceWeb.emit('log', JSON.parse(decompressed));
     });
 };
