fba54358d8c164640faa78185e2ffe226947439c
Rayim <tonite32@gmail.com>
2019-07-23 11:18:36 +0900

    update letter

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(methodOverride());
 app.set('trust proxy', 1);
 
 app.use(session({
-    secret: 'creeta',
+    secret: 'rayst',
     name: 'sessionId',
     resave: true,
     saveUninitialized: true,
